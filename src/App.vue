745f8565d8174cded707be848ff1f3ffbb61fbdf
VincenzoDiMasi <v.dimasi93@gmail.com>
2023-03-15 16:45:21 +0100

    Add appHeader

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,35 @@
 <script>
 import axios from 'axios';
+
+import AppHeader from './components/AppHeader.vue';
+const apiUri = 'http://localhost:8000/api/'
+export default {
+  name: 'App',
+  components: {AppHeader},
+  data() { 
+    return {
+      projects: []
+    }
+  },
+  methods: {
+    fetchProjects(){
+      axios.get(apiUri + 'projects').then(res => {
+        
+        this.projects = res.data;
+        
+      });
+    }
+  },
+  created() {
+    this.fetchProjects();
+  }
+}
 </script>
 
 <template>
-  
+<AppHeader></AppHeader>
+
+
 </template>
 
 <style lang="scss">
