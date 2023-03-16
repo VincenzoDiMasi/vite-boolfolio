0ea0565a543264ee75d962b20f86f29f8f078534
VincenzoDiMasi <v.dimasi93@gmail.com>
2023-03-16 17:11:07 +0100

    Add loader and created homepage

--- src/App.vue
+++ src/App.vue
@@ -3,22 +3,31 @@ import axios from 'axios';
 
 import AppHeader from './components/AppHeader.vue';
 import ProjectCard from './components/ProjectCard.vue';
+import AppLoader from './components/AppLoader.vue';
 const apiUri = 'http://localhost:8001/api/'
 export default {
   name: 'App',
-  components: {AppHeader, ProjectCard},
+  components: {AppHeader, ProjectCard, AppLoader},
   data() { 
     return {
-      projects: []
+      projects: [],
+      isLoading: false
     }
   },
   methods: {
     fetchProjects(){
+      this.isLoading = true;
       axios.get(apiUri + 'projects').then(res => {
         
         this.projects = res.data;
-        
-      });
+
+        }).catch(err => {
+          console.log(err);
+          
+        }).then(() => {
+          this.isLoading = false;
+          
+        });
     }
   },
   created() {
@@ -29,7 +38,9 @@ export default {
 
 <template>
 <AppHeader></AppHeader>
-<ProjectCard :projects="projects"></ProjectCard>   
+<ProjectCard :projects="projects"></ProjectCard>  
+<AppLoader v-if="isLoading"/>
+    <h1 v-else-if="!projects.length" class="text-center">No projects found!</h1> 
 
 
 </template>
---
+++ src/components/AppLoader.vue
@@ -0,0 +1,32 @@
+<script>
+export default {
+}
+</script>
+
+
+
+<template>
+
+<div class="spinner-container d-flex justify-content-center align-items-center">
+    <div class="spinner-border" role="status">
+    </div>
+</div>
+
+</template>
+
+
+
+<style scoped lang="scss">
+.spinner-container {
+    position: absolute;
+    top: 0;
+    right: 0;
+    left: 0;
+    bottom: 0;
+    background-color: rgba($color: #000000, $alpha: 0.5);
+}
+.spinner-border {
+    height: 200px;
+    width: 200px;
+}
+</style>
